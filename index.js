4e48bb673ddeb6df793f31296c95bf8669761173
Nanahawaw <folarinyetunde@gmail.com>
2023-09-22 13:54:41 +0100

    API integration

--- index.js
+++ index.js
@@ -1,68 +1,41 @@
-let now = new Date();
-let timeHour = now.getHours();
-let timeMin = now.getMinutes();
-
-let formattedTime = `${timeHour}:${timeMin.toString().padStart(2, "0")}`;
-let currentTime = document.querySelector(".time");
-currentTime.innerHTML = `${formattedTime}`;
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-
-let currentDay = document.querySelector(".monday");
-currentDay.innerHTML = `${day}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}: ${minutes}`;
+}
 
 function showTemperature(response) {
-  let temperature = response.data.main.temp;
-  console.log(temperature);
-  let temp = document.querySelector(".temp");
-  temp.innerHTML = `${temperature}`;
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
-  let description = response.data.weather[0].description;
-  let humidity = response.data.main.humidity;
-  let wind = response.data.wind.deg;
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = response.data.city;
 
-  let desc = document.querySelector(".desc");
-  desc.innerHTML = `${description}`;
+  let decriptionElement = document.querySelector("#description");
+  decriptionElement.innerHTML = response.data.condition.description;
 
-  let humid = document.querySelector("#humid");
-  humid.innerHTML = `Humidity ${humidity}%`;
+  let WindElement = document.querySelector("#wind");
+  WindElement.innerHTML = Math.round(response.data.wind.speed);
 
-  let winds = document.querySelector("#winds");
-  winds.innerHTML = `Wind ${wind} degrees`;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let inputValue = document.querySelector(".form-control").value;
-  let city = `${inputValue}`;
-  let cityDisplay = document.querySelector("h1");
-  cityDisplay.innerHTML = `${city}`;
-  let apiKey = "85eb492005f5eff1e67c2fd81be36e95";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
-
-let display = document.querySelector("#city-search-form");
-display.addEventListener("submit", searchCity);
-
-function getCurrentPosition(response) {
-  let temperature = response.data.main.temp;
-  console.log(temperature);
-}
-
-function currentLocation(event) {
-  alert(`It is ${temperature} degrees`);
-}
-let displayCurrent = document.querySelector("#current-location-btn");
-displayCurrent.addEventListener("click", currentLocation);
-
-navigator.geolocation.getCurrentPosition;
+let apiKey = "e6fcbcft268220745f113aof372ae233";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}`;
+axios.get(apiUrl).then(showTemperature);
